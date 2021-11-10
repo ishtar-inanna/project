0b8fcc7dceee3ca3ba151dd469aef592b113a5bf
ishtar-inanna <91423088+ishtar-inanna@users.noreply.github.com>
2021-11-10 19:12:36 +0200

    Add files via upload

---
+++ createProd.js
@@ -0,0 +1,34 @@
+let allProducts = [];
+
+function galaryShopList(product) {
+
+    let curentProduct = {name: n, image: i, testy: tT, description: s} = product;
+
+    allProducts.push(curentProduct);
+    console.log(allProducts);
+      
+}
+
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', () => {
+   let name = document.getElementById('fname').value;
+   
+   let image = "images/4972483.png";
+   
+   let testy = document.getElementById('tasty');
+   
+   let testyText = testy.options[testy.selectedIndex].text;
+   
+   let subject = document.getElementById('subject').value;
+
+   let newProduct = {
+        name: name,
+        image: image,
+        testy: testyText,
+        description: subject
+   }
+
+   galaryShopList(newProduct);
+
+});
